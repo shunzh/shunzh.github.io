49d8353a43216d811f8f93664d4ef47d76c04b5d
Shun Zhang <menie482@gmail.com>
2020-02-22 18:23:24 -0500

    need bib in convert

--- _posts/convert.sh
+++ _posts/convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pandoc -s -t markdown_strict+yaml_metadata_block --filter=pandoc-citeproc $1 -o $2-$1
+pandoc -s -t markdown_strict+yaml_metadata_block --filter=pandoc-citeproc --bibliography zotero.bib $1 -o $2-$1
