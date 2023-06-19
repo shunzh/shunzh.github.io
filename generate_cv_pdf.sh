54a7a39ea971ef84e5fd345521fcc2bf0a796b30
Shun Zhang <menie482@gmail.com>
2023-06-19 10:39:27 -0700

    modify tag style, add separator between works on mobile

--- generate_cv_pdf.sh
+++ generate_cv_pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 chrome_cmd="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-"$chrome_cmd" --headless --no-sandbox --no-pdf-header-footer --print-to-pdf=shun-zhang-cv.pdf _site/cv/index.html
+"$chrome_cmd" --headless --no-sandbox --no-pdf-header-footer --print-to-pdf=pdfs/shun-zhang-cv.pdf _site/cv/index.html
 
-git add shun-zhang-cv.pdf
+git add pdfs/shun-zhang-cv.pdf
