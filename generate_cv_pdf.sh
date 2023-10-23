224d201a79eeb7ac470b512f176fe56616571ab7
Shun Zhang <menie482@gmail.com>
2023-10-22 22:03:38 -0700

    mc

--- generate_cv_pdf.sh
+++ generate_cv_pdf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-pagedjs-cli _site/cv/index.html -o pdfs/shun-zhang-cv.pdf
+git diff --name-only --cached | grep 'cv.md'
+if [ $? -eq 0 ]; then
+  echo "cv.md is updated, regenerating pdf..."
+  pagedjs-cli _site/cv/index.html -o pdfs/shun-zhang-cv.pdf
 
-git add pdfs/shun-zhang-cv.pdf
+  git add pdfs/shun-zhang-cv.pdf
+fi
